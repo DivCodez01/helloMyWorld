346292158b8c0c1b40f5e68bfc42d6d89fa18ce1
divin-gakiza <divin.gakiza01@gmail.com>
2024-05-01 21:50:15 +0200

    finish the image slide

--- chnageTheImages.js
+++ chnageTheImages.js
@@ -1,21 +1,31 @@
 document.title = "IMAGE SLIDE"
 
 const imgs = [
-    "IMG1.JPG",
-    "IMG1.JPG",
-    "IMG1.JPG",
-    "IMG1.JPG"
-];
+    "IMG1",
+    "IMG2",
+    "IMG3",
+    "IMG4"
+]
 
 const img = document.querySelector("img");
 const label = document.querySelector("label");
 
-let src = `${imgs[0]}`
 let change = 0;
+let src = `${imgs[change]}.JPG`;
+console.log(src)
 img.src = src;
 img.setAttribute("alt", "my Image")
 console.log(img)
 
 label.addEventListener("click", function () {
-
+    change += 1;
+    src = `${imgs[change]}.JPG`;
+    img.src = src;
+    console.log(img)
+    console.log(change)
+    if (change - 1 == imgs.length - 2) {
+        change = -1;
+        src = `${imgs[change]}.JPG`;
+        console.log("good")
+    }
 })
