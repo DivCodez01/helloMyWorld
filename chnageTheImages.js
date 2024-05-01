e7c647336a065f59d5a073e1324fd510aa185999
divin-gakiza <divin.gakiza01@gmail.com>
2024-05-01 21:26:16 +0200

    myImage

--- chnageTheImages.js
+++ chnageTheImages.js
@@ -1,1 +1,21 @@
 document.title = "IMAGE SLIDE"
+
+const imgs = [
+    "IMG1.JPG",
+    "IMG1.JPG",
+    "IMG1.JPG",
+    "IMG1.JPG"
+];
+
+const img = document.querySelector("img");
+const label = document.querySelector("label");
+
+let src = `${imgs[0]}`
+let change = 0;
+img.src = src;
+img.setAttribute("alt", "my Image")
+console.log(img)
+
+label.addEventListener("click", function () {
+
+})
