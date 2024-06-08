c21793843e7ba2d3373ad625a9890934344c115d
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-08 23:43:27 +0200

    js doc element and file

---
+++ bookStore.js
@@ -0,0 +1,4 @@
+const pageBanner = document.getElementById("page-banner");
+const addBook = document.getElementById("add-book");
+const books = document.querySelector("#book-list li");
+const deleteBtn = document.querySelector("#book-list .delete");
