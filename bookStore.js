31b378a574028dfb7ec049ac802e4c8d34d9989b
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-09 01:17:29 +0200

    delete book in store

--- bookStore.js
+++ bookStore.js
@@ -1,18 +1,42 @@
 const pageBanner = document.getElementById("page-banner");
 const books = document.querySelector("#book-list ul");
-const deleteBtn = document.querySelector("#book-list .delete");
+const deleteBtn = document.querySelectorAll("#book-list .delete");
 const form = document.forms.addBook;
 
-form.addBtn.onclick = (ev) => {
-    ev.preventDefault()
-    let inputValue = form.input.value;
 
-    if (inputValue) {
-        books.innerHTML += `
-        <li>
-            <span class='name'>${inputValue}</span>
-            <span class='delete'>delete</span>
-        </li>`
-    }
+form.addBtn.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    addnewElement();
+})
+
+function addnewElement() {
+
+    let inputValue = form.input.value;
+    const list = document.createElement("li");
+    const nameList = document.createElement("span");
+    const deleteList = document.createElement("span");
+
+    // if (inputValue) {
+    nameList.classList.add('name');
+    deleteList.classList.add('delete');
+
+    nameList.innerHTML = inputValue;
+    deleteList.innerHTML = 'delete';
+
+    list.appendChild(nameList);
+    list.appendChild(deleteList);
+    books.appendChild(list);
+
+    // }
     form.input.value = '';
 }
+
+books.addEventListener("click", function (ev) {
+
+    if (ev.target.className == 'delete') {
+        // ev.target.parentElement.remove();
+        const li = ev.target.parentElement;
+        // li.parentNode.removeChild(li);
+        li.remove()
+    }
+})
