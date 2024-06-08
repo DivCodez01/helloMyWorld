31d20b513620ee8afd84803e200f42e6b4d2d086
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-09 00:02:12 +0200

    adding new li elemt

--- bookStore.js
+++ bookStore.js
@@ -1,7 +1,18 @@
 const pageBanner = document.getElementById("page-banner");
-const books = document.querySelector("#book-list li");
+const books = document.querySelector("#book-list ul");
 const deleteBtn = document.querySelector("#book-list .delete");
+const form = document.forms.addBook;
 
-document.forms.addBook.addBtn.onclick = (ev) => {
+form.addBtn.onclick = (ev) => {
     ev.preventDefault()
+    let inputValue = form.input.value;
+
+    if (inputValue) {
+        books.innerHTML += `
+        <li>
+            <span class='name'>${inputValue}</span>
+            <span class='delete'>delete</span>
+        </li>`
+    }
+    form.input.value = '';
 }
